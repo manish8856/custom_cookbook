b3533a64a25799b7506bd6c9f25c6a54a6493dcb
manish8856 <manish.kumar@crichq.com>
2016-08-11 10:25:47 +0530

    Fist Commit

---
+++ custom/metadata.rb
@@ -0,0 +1,7 @@
+name 'custom'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures custom'
+long_description 'Installs/Configures custom'
+version '0.1.0'
---
+++ custom/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: custom
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
